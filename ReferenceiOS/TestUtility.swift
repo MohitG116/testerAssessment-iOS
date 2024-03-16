c49f4eab78c011011c03025d064bac73af77620e
mohigarg116@gmail.com <mohitgarg116@gmail.com>
2024-03-17 00:13:19 +0100

    Added the UI and Unit Test cases

---
+++ ReferenceiOS/TestUtility.swift
@@ -0,0 +1,37 @@
+//
+//  Helper.swift
+//  ReferenceiOS
+//
+//  Created by User1 on 16/03/24.
+//  Copyright © 2024 ABN AMRO. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    func numberfromString() -> NSNumber? {
+        let formatter = NumberFormatter()
+        formatter.locale = Locale(identifier: "nl_NL")
+        formatter.numberStyle = .currency
+        formatter.maximumFractionDigits = 2
+        
+        return formatter.number(from: self) ?? 0.0
+    }
+    
+    func matches(_ regex: String) -> Bool {
+        return self.range(of: regex, options: .regularExpression, range: nil, locale: nil) != nil
+    }
+}
+
+extension NSNumber {
+    func stringfromNumber() -> String? {
+        let formatter = NumberFormatter()
+        formatter.locale = Locale(identifier: "nl_NL")
+        formatter.numberStyle = .currency
+        formatter.maximumFractionDigits = 2
+        
+        return formatter.string(from: self) ?? "€ 10,0"
+    }
+}
+
